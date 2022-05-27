f1769324f0803f84d03837739a5b1d6a12054616
Elvis <Elvis1327@gmail.com>
2022-05-26 20:46:12 -0400

    added styles core of swiper js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { NetflixApp } from './NetflixApp';
 
+import 'swiper/swiper.min.css';
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/components/navigation/navigation.min.css'
+import 'swiper/components/pagination/pagination.min.css'
+
+import './styles/styles.scss';
+
 const main = document.querySelector('#root');
-
-
 render(
     <NetflixApp />,
     main
